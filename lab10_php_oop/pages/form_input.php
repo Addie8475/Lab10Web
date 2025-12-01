6aa876f9f35037ad53e87628137f5ff1b296c6a3
Addie8475 <dezaauliaaditya@gmail.com>
2025-12-01 20:01:36 +0700

    Update form_input.php

--- lab10_php_oop/pages/form_input.php
+++ lab10_php_oop/pages/form_input.php
@@ -3,18 +3,25 @@ include "../classes/Form.php";
 ?>
 
 <html>
-<head><title>Input Mahasiswa</title></head>
+<head>
+    <title>Input Mahasiswa</title>
+    <link rel="stylesheet" href="../assets/form.css">
+</head>
 <body>
 
-<h3>Silahkan isi data mahasiswa:</h3>
+<div class="form-container">
+    <h3>Input Data Mahasiswa</h3>
 
-<?php
-$form = new Form("save_data.php", "Simpan Data");
-$form->addField("nim", "NIM");
-$form->addField("nama", "Nama");
-$form->addField("alamat", "Alamat");
-$form->display();
-?>
+    <?php
+    $form = new Form("save_data.php", "Simpan Data");
+    $form->addField("nim", "NIM");
+    $form->addField("nama", "Nama");
+    $form->addField("alamat", "Alamat");
+    $form->display();
+    ?>
+
+    <a class="back-btn" href="dashboard.php">← Kembali ke Dashboard</a>
+</div>
 
 </body>
 </html>
